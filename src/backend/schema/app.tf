91ba7019eed4cd53ce31dff655262919ffc9a953
humberd <humberd2@gmail.com>
2020-12-27 14:43:49 +0100

    Attached cluster to a project

--- src/backend/schema/app.tf
+++ src/backend/schema/app.tf
@@ -25,9 +25,18 @@ resource "digitalocean_kubernetes_cluster" "blog-dev" {
   name = "blog-dev"
   region = local.region
   version = "1.19.3-do.2"
+
   node_pool {
     name = "worker-pool"
     size = "s-1vcpu-2gb"
     node_count = 2
   }
+}
+
+
+resource "digitalocean_project_resources" "blog-dev-resources" {
+  project = digitalocean_project.blog-dev.id
+  resources = [
+    "do:kubernetes:${digitalocean_kubernetes_cluster.blog-dev.id}"
+  ]
 }
